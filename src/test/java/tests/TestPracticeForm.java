99bcb4beb9d48c9feee80cf3fc517e4b3fc38f77
alinaiakovleva <yalina905@gmail.com>
2026-07-25 12:53:56 +0200

    Added TestData class

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -1,65 +1,67 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.devtools.v131.filesystem.model.File;
+import tests.testdata.TestData;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static tests.testdata.TestData.*;
 
 public class TestPracticeForm extends TestBase {
 
     @Test
     void successfulSubmitAllFieldsTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Alina");
-        $("[id=lastName]").setValue("Test");
-        $("[id=userEmail]").setValue("test@mail.com");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("1234567891");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userEmail]").setValue(userEmail);
+        $("#genterWrapper").$(byText(genderFemale)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("[id=dateOfBirthInput]").click();
-        $("[class=react-datepicker__year-select]").selectOption("1991");
-        $("[class=react-datepicker__month-select]").selectOption("September");
-        $(".react-datepicker__day.react-datepicker__day--016").click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("[class=react-datepicker__year-select]").selectOption(yearOfBirth);
+        $("[class=react-datepicker__month-select]").selectOption(monthOfBirth);
+        $(".react-datepicker__day.react-datepicker__day--0" + dayOfBirth).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
         $("#uploadPicture").uploadFromClasspath("test.jpg");
-        $("#currentAddress").setValue("Moscow, Lermontova street, 25-70");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
 
         //check submitted values
-        $(".table-responsive").shouldHave(text("Alina"));
-        $(".table-responsive").shouldHave(text("Test"));
-        $(".table-responsive").shouldHave(text("test@mail.com"));
-        $(".table-responsive").shouldHave(text("Female"));
-        $(".table-responsive").shouldHave(text("1234567891"));
-        $(".table-responsive").shouldHave(text("1991"));
-        $(".table-responsive").shouldHave(text("September"));
-        $(".table-responsive").shouldHave(text("16"));
-        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text(firstName));
+        $(".table-responsive").shouldHave(text(lastName));
+        $(".table-responsive").shouldHave(text(userEmail));
+        $(".table-responsive").shouldHave(text(genderFemale));
+        $(".table-responsive").shouldHave(text(userNumber));
+        $(".table-responsive").shouldHave(text(yearOfBirth));
+        $(".table-responsive").shouldHave(text(monthOfBirth));
+        $(".table-responsive").shouldHave(text(String.valueOf(TestData.dayOfBirth)));
+        $(".table-responsive").shouldHave(text(subject));
+        $(".table-responsive").shouldHave(text(hobby));
         $(".table-responsive").shouldHave(text("test.jpg"));
-        $(".table-responsive").shouldHave(text("Moscow, Lermontova street, 25-70"));
-        $(".table-responsive").shouldHave(text("NCR"));
-        $(".table-responsive").shouldHave(text("Delhi"));
+        $(".table-responsive").shouldHave(text(currentAddress));
+        $(".table-responsive").shouldHave(text(state));
+        $(".table-responsive").shouldHave(text(city));
     }
 
     @Test
     void successfulSubmitRequiredFieldsTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Alina");
-        $("[id=lastName]").setValue("Test");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("1234567891");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userNumber]").setValue(userNumber);
+        $("#genterWrapper").$(byText(genderFemale)).click();
         $("#submit").click();
 
         //check submitted values
-        $(".table-responsive").shouldHave(text("Alina"));
-        $(".table-responsive").shouldHave(text("Test"));
-        $(".table-responsive").shouldHave(text("1234567891"));
-        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text(firstName));
+        $(".table-responsive").shouldHave(text(lastName));
+        $(".table-responsive").shouldHave(text(userNumber));
+        $(".table-responsive").shouldHave(text(genderFemale));
     }
 
     @Test
@@ -74,41 +76,41 @@ public class TestPracticeForm extends TestBase {
     @Test
     void phoneFieldValidationTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Alina");
-        $("[id=lastName]").setValue("Test");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("1234567");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("#genterWrapper").$(byText(genderFemale)).click();
+        $("[id=userNumber]").setValue(userInvalidNumber);
         $("#submit").click();
 
         //check phone field validation
         $(".modal-content").shouldNot(exist);
-        $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#userNumber").shouldHave(cssValue(borderColor, borderColorRGB));
     }
 
     @Test
     void firstNameFieldValidationTest() {
         open("/automation-practice-form");
-        $("[id=lastName]").setValue("Test");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("1234567890");
+        $("[id=lastName]").setValue(lastName);
+        $("#genterWrapper").$(byText(genderFemale)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("#submit").click();
 
         //check first name field validation
         $(".modal-content").shouldNot(exist);
-        $("#firstName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#firstName").shouldHave(cssValue(borderColor, borderColorRGB));
     }
 
     @Test
     void lastNameFieldValidationTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Alina");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("1234567890");
+        $("[id=firstName]").setValue(firstName);
+        $("#genterWrapper").$(byText(genderFemale)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("#submit").click();
 
         //check last name field validation
         $(".modal-content").shouldNot(exist);
-        $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#lastName").shouldHave(cssValue(borderColor, borderColorRGB));
     }
 
 }
---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,25 @@
+package tests.testdata;
+
+public class TestData {
+    // Test Data for PracticeForm
+    public static String firstName = "Alina";
+    public static String lastName = "Test";
+    public static String userEmail = "test@mail.com";
+    public static String genderFemale = "Female";
+    public static String userNumber = "1234567891";
+    public static String yearOfBirth = "1991";
+    public static String monthOfBirth = "September";
+    public static int dayOfBirth = 16;
+    public static String subject = "English";
+    public static String hobby = "Sports";
+    public static String currentAddress = "Moscow, Lermontova street, 25-70";
+    public static String state = "NCR";
+    public static String city = "Delhi";
+
+    // Invalid Test Data
+    public static String userInvalidNumber = "1234567";
+
+    //Styles Field Validation
+    public static String borderColor = "border-color";
+    public static String borderColorRGB = "rgb(220, 53, 69)";
+}
